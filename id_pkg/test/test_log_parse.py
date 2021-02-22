3544737fe463a19bb988dada70a8a7bb0df73b49
austin3dprinting <austin@austingeorge.com>
2021-02-21 20:12:30 -0500

    Added %ASA-3-114018: Failed to set port speed in 4GE SSM I/O card (error error_string).

--- id_pkg/test/test_log_parse.py
+++ id_pkg/test/test_log_parse.py
@@ -69,6 +69,13 @@ class LogParseTest(unittest.TestCase):
         self.assertEqual('Asynchronous error: error_message', df.loc[326028, 'Text'])
         self.assertEqual('error_message', df.loc[326028, 'Error'])
 
+        # %ASA-3-114018: Failed to set port speed in 4GE SSM I/O card (error error_string).
+        self.assertTrue(df.loc[114018, 'Type'] == 'ASA')
+        self.assertTrue(df.loc[114018, 'Severity'] == 3)
+        self.assertTrue(df.loc[114018, 'Text'] == 'Failed to set port speed in 4GE SSM I/O card (error error_string).')
+        self.assertTrue(df.loc[114018, 'Error'] == 'error_string')
+
+
 
 if __name__ == '__main__':
     unittest.main()
